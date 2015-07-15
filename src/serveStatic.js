b6e39d8357ff55635a81b70193dbb69290b68bed
Jason Reynolds <dhyasama@gmail.com>
2015-07-14 21:57:26 -0400

    serve static files

---
+++ src/serveStatic.js
@@ -0,0 +1,37 @@
+"use strict";
+
+module.exports = function (app) {
+
+  var path = require('path');
+
+  var sendFile = function(root, filename, res) {
+    return res.sendFile(filename, {
+      root: root
+    }, function (err) {
+      if (err) {
+        console.log(err);
+        res.status(err.status).end();
+      }
+      else {
+        console.log('Sent:', root + filename);
+      }
+    });
+  };
+
+  app.get('/', function (req, res) {
+    return sendFile(path.join(__dirname, '../static/'), 'index.html', res);
+  });
+
+  app.get('/js/:name', function (req, res) {
+    return sendFile(path.join(__dirname, '../js/'), req.params.name, res);
+  });
+
+  app.get('/lib/:name', function (req, res) {
+    return sendFile(path.join(__dirname, '../lib/'), req.params.name, res);
+  });
+
+  app.get('/assets/:name', function (req, res) {
+    return sendFile(path.join(__dirname, '../assets/'), req.params.name, res);
+  });
+
+};
--- src/server.js
+++ src/server.js
@@ -1,20 +1,10 @@
-/**
- * Created by jay on 5/30/15.
- */
+"use strict";
 
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
+var serveStatic = require('./serveStatic')(app);
 var Starcoder = require('./Starcoder-server.js');
-
-//io.on('connect', function (socket) {
-//    console.log('connect', socket.id);
-//    socket.on('ready', function () {
-//        socket.emit('add ship', {x: 0, y: 0, id: socket.id});
-//    });
-//});
-
 var starcoder = new Starcoder(app, io);
 
 server.listen(8081);
