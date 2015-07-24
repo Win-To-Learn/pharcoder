b1086de92ad94ec2d73946e45409f668522cf52c
Jason Reynolds <dhyasama@gmail.com>
2015-07-24 13:46:58 -0400

    serve new static files

--- src/serveStatic.js
+++ src/serveStatic.js
@@ -22,8 +22,20 @@ module.exports = function (app) {
     return sendFile(path.join(__dirname, '../'), 'index.html', res);
   });
 
+  app.get('/blockly.html', function (req, res) {
+    return sendFile(path.join(__dirname, '../'), 'blockly.html', res);
+  });
+
+  app.get('/src/BlocklyCustom.js', function (req, res) {
+    return sendFile(path.join(__dirname, '../'), '/src/BlocklyCustom.js', res);
+  });
+
   app.get('/css/:name', function (req, res) {
     return sendFile(path.join(__dirname, '../css/'), req.params.name, res);
+  });
+
+  app.get('/css/images/:name', function (req, res) {
+    return sendFile(path.join(__dirname, '../css/images/'), req.params.name, res);
   });
 
   app.get('/js/:name', function (req, res) {
@@ -34,12 +46,20 @@ module.exports = function (app) {
     return sendFile(path.join(__dirname, '../lib/msg/json/'), req.params.name, res);
   });
 
+  app.get('/lib/msg/js/:name', function (req, res) {
+    return sendFile(path.join(__dirname, '../lib/msg/js/'), req.params.name, res);
+  });
+
   app.get('/lib/:name', function (req, res) {
     return sendFile(path.join(__dirname, '../lib/'), req.params.name, res);
   });
 
   app.get('/assets/sounds/:name', function (req, res) {
     return sendFile(path.join(__dirname, '../assets/sounds/'), req.params.name, res);
+  });
+
+  app.get('/assets/bitmapfonts/:name', function (req, res) {
+    return sendFile(path.join(__dirname, '../assets/bitmapfonts/'), req.params.name, res);
   });
 
   app.get('/assets/:name', function (req, res) {
