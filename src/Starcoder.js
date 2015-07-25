67510108e714d28ee21679e10a94444fe8de4215
Jason Reynolds <dhyasama@gmail.com>
2015-07-25 08:15:16 -0400

    add port to server uri

--- src/Starcoder.js
+++ src/Starcoder.js
@@ -15,7 +15,7 @@
 
 var config = {
     version: '0.1',
-    serverUri: 'http://pharcoder-single-1.elasticbeanstalk.com',
+    serverUri: 'http://pharcoder-single-1.elasticbeanstalk.com:8081',
     //serverUri: 'http://localhost:8081',
     //worldBounds: [-4200, -4200, 8400, 8400],
     worldBounds: [-200, -200, 200, 200],
