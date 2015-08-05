5b460182fbf6dff15fdeb73bee4cc4e860f4a10f
Jason Reynolds <dhyasama@gmail.com>
2015-08-05 19:58:19 -0400

    make cors headers more specific

--- src/server.js
+++ src/server.js
@@ -9,9 +9,10 @@ var Starcoder = require('./Starcoder-server.js');
 //var argv = require('minimist')(process.argv.slice(2));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization, Content-Length, Accept, X-Requested-With, *");
+  next();
 });
 
 var starcoder = new Starcoder(app, io);
