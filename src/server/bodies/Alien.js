eb30390caa1c74a6b1192d4df2af2633173355e6
Jay Bloodworth <johnabloodworth3@gmail.com>
2016-06-10 21:47:22 -0400

    More alien AI updates

--- src/server/bodies/Alien.js
+++ src/server/bodies/Alien.js
@@ -11,21 +11,33 @@ var FSM = require('../util/FSM.js');
 
 //var ALIEN_THRUST_FORCE = 150;
 var ALIEN_ROTATION_FORCE = 50;
-var ALIEN_SCAN_RADIUS = 75;
+var ALIEN_SCAN_RADIUS = 150;
+
+
+var _routes = [
+    [[0.125,0.125],[0.875,0.25],[0.125,0.375],[0.875,0.5],[0.125,0.625],[0.875,0.75],[0.125,0.875]],
+    [[0.875,0.125],[0.125,0.25],[0.875,0.375],[0.125,0.5],[0.875,0.625],[0.125,0.75],[0.875,0.875]],
+    [[0.125,0.125],[0.25,0.875],[0.375,0.125],[0.5,0.875],[0.625,0.125],[0.75,0.875],[0.875,0.125]],
+    [[0.125,0.875],[0.25,0.125],[0.375,0.875],[0.5,0.125],[0.625,0.875],[0.75,0.125],[0.875,0.875]]
+];
 
 var Alien = function (starcoder, config) {
     SyncBodyBase.call(this, starcoder, config);
     this.damping = 0.75;
     this.angularDamping = .25;
-    this.targetShip = null;
+    this.target = null;
     //this.setGoal();
+    this.frustration = 0;
+    this.route = _routes[Math.floor(Math.random()*_routes.length)];
+    this.routePos = Math.floor(Math.random()*this.route.length);
+    this.setGoal();
     this.proximitySensor = new p2.Circle({radius: ALIEN_SCAN_RADIUS, sensor: true});
     this.setCollisionGroup(this.proximitySensor);
     this.setCollisionMask(this.proximitySensor, [this.targetType]);
     this.addShape(this.proximitySensor);
     this.brain = new AlienBrain(this.patience, this.persistence);
     //this.brain.on('plotting', function () {console.log('plot state')});
-    this.brain.on('plotting', this.setGoal.bind(this));
+    //this.brain.on('plotting', this.setGoal.bind(this));
     this.brain.transition('start');
 };
 
@@ -55,11 +67,15 @@ Alien.prototype.control = function () {
         var dx = this.target.position[0] - this.position[0];
         var dy = this.target.position[1] - this.position[1];
     } else {
+        if (this.frustration >= 10) {
+            this.frustration = 0;
+            this.setGoal();
+        }
         dx = this.goal.x - this.position[0];
         dy = this.goal.y - this.position[1];
         if ((dx*dx + dy*dy) <= 4) {
-            //this.setGoal();
-            this.brain.transition('reached goal');
+            this.setGoal();
+            //this.brain.transition('reached goal');
         }
     }
     var n = this.thrustForce / Math.sqrt(dx * dx + dy * dy);
@@ -71,8 +87,11 @@ Alien.prototype.control = function () {
  */
 Alien.prototype.setGoal = function () {
     this.target = null;
-    var rx = (Math.floor(Math.random()*7) + 1) * 0.125;
-    var ry = (Math.floor(Math.random()*7) + 1) * 0.125;
+    this.routePos = (this.routePos + 1) % this.route.length;
+    var rx = this.route[this.routePos][0];
+    var ry = this.route[this.routePos][1];
+    //var rx = (Math.floor(Math.random()*7) + 1) * 0.125;
+    //var ry = (Math.floor(Math.random()*7) + 1) * 0.125;
     this.goal = {x: Math.floor(this.starcoder.worldLeft + rx*this.starcoder.worldWidth),
         y: Math.floor(this.starcoder.worldBottom + ry*this.starcoder.worldHeight)};
     //console.log('SC', this.starcoder.worldLeft, this.starcoder.worldWidth, this.starcoder.worldTop, this.starcoder.worldHeight);
@@ -99,6 +118,14 @@ Alien.prototype.beginContact = function (body) {
             this.dead = true;
             this.worldapi.removeSyncableBody(body);
             this.setTimer(5, {fun: this.worldapi.respawn.bind(this.world, this, {position: {random: 'world'}})});
+            break;
+        case 'HydraArm':
+        case 'Planetoid':
+            this.frustration += 5;
+            break;
+        case 'Crystal':
+        case 'Asteroid':
+            this.frustration += 1;
             break;
     }
 };
