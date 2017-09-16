7fa27a6c5e21294713b7a23518dd6a25b0e21993
Jay Bloodworth <johnabloodworth3@gmail.com>
2017-09-15 21:11:54 -0400

    Initial check in of Turret object code (copy paste from StationBlock)

---
+++ src/common/bodies/Turret.js
@@ -0,0 +1,22 @@
+/**
+ * Turret.js
+ *
+ * Shared client / server
+ */
+'use strict';
+
+module.exports = {
+    proto: {
+        _lineColor: '#6699cc',
+        _fillColor: '#000000',
+        _lineWidth: 4,
+        _fillAlpha: 0,
+        _shapeClosed: true
+    },
+
+    updateProperties: {
+        lineColor : 'string',
+        vectorScale : 'ufixed16',
+        shape : 'pairarrayfixed16'}
+};
+
