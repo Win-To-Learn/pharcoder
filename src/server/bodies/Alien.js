6215c387225a4b36e40802b62aeae76729015378
wintolearn <jmartin@wintolearn.com>
2017-09-22 22:12:08 -0400

    added portuguese
    added portuguese
    
    slowed down blockbuster aliens

--- src/server/bodies/Alien.js
+++ src/server/bodies/Alien.js
@@ -53,7 +53,7 @@ Alien.prototype.persistence = 10000;
 var genera = [
     {name: 'Warrior', props: {thrustForce: 250, targetType: 'Ship', lineColor: '#ffa500'}},
     {name: 'EcoTerrorist', props: {thrustForce: 200, targetType: 'Tree', lineColor: '#ffcc99', patience: 10000}},
-    {name: 'BlockBuster', props: {thrustForce: 300, targetType: 'StationBlock', lineColor: '#ffff00', patience: 15000}}
+    {name: 'BlockBuster', props: {thrustForce: 100, targetType: 'StationBlock', lineColor: '#ffff00', patience: 10000}}
 ];
 
 SyncBodyBase.applyGenera(Alien, genera);
